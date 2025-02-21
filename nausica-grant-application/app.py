d375941c260de6e514c7543ad53ba9e77190be73
l00188372 <l00188372@atu.ie>
2025-02-21 14:33:12 +0000

    Added swagger

--- nausica-grant-application/app.py
+++ nausica-grant-application/app.py
@@ -1,16 +1,36 @@
-"""Basic Flask application for rendering web pages."""
 from flask import Flask, render_template
+from flasgger import Swagger
 
 app = Flask(__name__)
-
+swagger = Swagger(app)
 
 @app.route("/")
 def home():
-    """Render the home page using the 'home.html' template."""
+    """
+    Home Page
+    ---
+    tags:
+      - Pages
+    summary: Render the home page
+    description: Renders the home page using the 'home.html' template.
+    responses:
+      200:
+        description: Successfully rendered home page.
+    """
     return render_template("home.html")
 
 
 @app.route("/about")
 def about():
-    """Render the about page using the 'about.html' template."""
+    """
+    About Page
+    ---
+    tags:
+      - Pages
+    summary: Render the about page
+    description: Renders the about page using the 'about.html' template.
+    responses:
+      200:
+        description: Successfully rendered about page.
+    """
     return render_template("about.html")
